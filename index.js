722a823232a798659a4810f0af630fafa27f83d8
SahsiSunny <sahsisunny@gmail.com>
2023-01-23 13:31:01 +0530

    Revert "added some routes"
    This reverts commit ef07b877976f0230de4ecbb6469befdcb5eec77a.

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/users/:name', (req, res) => {  
-  res.send(`User name is ${req.params.name}`);
-});
-
-app.route('/book')
-  .get((req, res) => {
-    res.send('Get a random book');
-  })
-  .post((req, res) => {
-    res.send('Add a book');
-  })
-  .put((req, res) => {
-    res.send('Update the book');
-  });
-
-
 app.listen(process.env.PORT || 3500, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT || 3500}`);
-});
+    console.log('Server Started');
+  });
