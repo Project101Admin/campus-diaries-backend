ef07b877976f0230de4ecbb6469befdcb5eec77a
SahsiSunny <sahsisunny@gmail.com>
2023-01-23 13:30:13 +0530

    added some routes

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 3500, () => {
-    console.log('Server Started');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/users/:name', (req, res) => {  
+  res.send(`User name is ${req.params.name}`);
+});
+
+app.route('/book')
+  .get((req, res) => {
+    res.send('Get a random book');
+  })
+  .post((req, res) => {
+    res.send('Add a book');
+  })
+  .put((req, res) => {
+    res.send('Update the book');
   });
+
+
+app.listen(process.env.PORT || 3500, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT || 3500}`);
+});
