4ef249ef9fda61c947c96d5e11cc37f6c437ac21
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-01-30 18:22:03 +0530

    setup mongo

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const DB = process.env.DATABASE;
+const DB = 'mongodb+srv://priyanshu:appyfizz@cluster0.4arzow6.mongodb.net/backend?retryWrites=true&w=majority';
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
-mongoose.connect(DB).then(() => {
-  console.log('connection successful')
-}).catch((err) => console.log('no connection'));
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('connection successful');
+  })
+  .catch((err) => console.log('no connection'));
