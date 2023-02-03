87db32940fcb734cc13264bbcc92186be9834819
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-02-03 23:17:11 +0530

    csv successfully uploaded to database

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,48 @@
 const express = require('express');
-const user = express();
 const router = express.Router();
 const multer = require('multer');
-const path = require('path');
+const { csvArrayToJSON } = require('../common/utils');
+var csv = require('csvtojson');
+var userSchema = require('../model/userSchema');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, '../assets');
-    console.log('saved');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
+
 router.post('/importUser', upload.single('file'), async (req, res) => {
   try {
-    res.send({ status: 200, message: 'success' });
+    const csvRow = await csv({
+      noheader: true,
+      output: 'csv',
+    }).fromString(req.file.buffer.toString());
+    const csvJson = await csvArrayToJSON(csvRow);
+    // console.log(csvJson);
+    var user = [];
+    for (var i = 0; i < csvJson.length; i++) {
+      var obj = {};
+      obj.name = csvJson[i]['Name'];
+      obj.phone = csvJson[i]['PhoneNo'];
+      obj.rollNo = csvJson[i]['RollNo'];
+      user.push(obj);
+    }
+    console.log(user);
+    try {
+      const uploaded = await userSchema.insertMany(user);
+      res.status(200).send({
+        message: 'Successfully Uploaded!',
+        uploaded,
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: 'failure',
+        error,
+      });
+    }
   } catch (err) {
-    res.send({ message: 'got error' });
+    res.status(500).send({
+      message: err.message,
+      err,
+    });
   }
 });
 
