420ff1eb407a76f312d7f6a85612a5478f397c25
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-02-04 22:38:42 +0530

    my file

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const DB = process.env.DATABASE;
+const DB = process.env.MONGO_URL;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
-mongoose.connect(DB).then(() => {
-  console.log('connection successful')
-}).catch((err) => console.log('no connection'));
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('connection successful');
+  })
+  .catch((err) => console.log('no connection'));
