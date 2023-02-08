5d212f3fce4eac205d30b4eecf41557c8ca1472a
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-02-08 14:35:24 +0530

    Add manual and delete route

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const multer = require('multer');
 const { csvArrayToJSON } = require('../common/utils');
 const csv = require('csvtojson');
+
+require('../database/connection');
 const userSchema = require('../model/userSchema');
 
 const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
 
-router.post('/importUser', upload.single('file'), async (req, res) => {
+router.post('/importCsv', upload.single('file'), async (req, res) => {
   try {
     const csvRow = await csv({
       noheader: true,
@@ -46,8 +48,42 @@ router.post('/importUser', upload.single('file'), async (req, res) => {
   }
 });
 
-router.get('/', (req, res) => {
-  res.send(`hello admin`);
+router.post('/importManual', async (req, res) => {
+  const { name, phone, rollNo } = req.body;
+  if (!name || !phone || !rollNo) {
+    return res.status(422).json({ error: 'Plz filled the field properly' });
+  }
+
+  try {
+    const userExist = await userSchema.findOne({ rollNo: rollNo });
+    if (userExist) {
+      return res.status(422).json({ error: 'RollNo already Exist' });
+    }
+    const user = new userSchema({ name, phone, rollNo });
+    await user.save();
+    res.status(201).json({ message: 'user registered successfuly' });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete('/user/:id', async (req, res) => {
+  try {
+    const user = await userSchema.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send('User not found');
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.get('/user', (req, res) => {
+  userSchema.find({}, (err, users) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send(users);
+  });
 });
 
 router.post('/addStudents', (req, res) => {
